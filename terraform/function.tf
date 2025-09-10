326207976a302d8234dd791f5b437ac5e83b4268
Nahum Villanueva Cardenas <nahumvillanuevacardenas@gmail.com>
2025-09-10 11:59:23 -0700

    Delete terraform directory

--- terraform/function.tf
+++
@@ -1,6 +0,0 @@
-# Create file in a local machine using Terraform
-resource "null_resource" "file" {
-    provisioner "local-exec" {
-        command = "echo 'Message: ${upper("hello world!")}' > hello.txt "
-    }
-}
